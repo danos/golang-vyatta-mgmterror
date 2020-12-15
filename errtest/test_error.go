8bee857b50deedaa1860ec5b1123c82c5f128b5e
William Ivory <william.ivory@intl.att.com>
2020-12-15 11:03:28 +0000

    Combine and export errpath functions to ErrPath
    Part of VRVDR-53854.  errpath() is duplicated in several places, and would be useful if exported.  So, combine the 2 local versions here and export it.
    
    (cherry picked from commit 935b3405f29a18116577cc0d51119369372ca90a)

--- errtest/test_error.go
+++ errtest/test_error.go
@@ -206,7 +206,7 @@ func (te *TestError) CliErrorStrings() []string {
 
 	pathSlice := getPathSlice(te.t, te.path, "generic error")
 
-	retStr := []string{fmt.Sprintf("%s", errpath(pathSlice))}
+	retStr := []string{fmt.Sprintf("%s", mgmterror.ErrPath(pathSlice))}
 	return append(retStr, te.cliMsgs...)
 }
 
@@ -222,7 +222,7 @@ func (te *TestError) RpcErrorStrings() []string {
 
 	pathSlice := getPathSlice(te.t, te.path, "rpc error")
 
-	retStr := []string{fmt.Sprintf("%s", errpath(pathSlice))}
+	retStr := []string{fmt.Sprintf("%s", mgmterror.ErrPath(pathSlice))}
 	return append(retStr, te.rpcMsgs...)
 }
 
@@ -243,18 +243,18 @@ func (te *TestError) SetCliErrorStrings() []string {
 	pathSlice := getPathSlice(te.t, te.path, "generic error")
 	if te.setMsg == noMsgPrinted {
 		return []string{fmt.Sprintf("%s %s %s",
-			configPathStr, errpath(pathSlice), isNotValidStr),
+			configPathStr, mgmterror.ErrPath(pathSlice), isNotValidStr),
 		}
 	}
 	if te.setSuffix == "" {
 		return []string{fmt.Sprintf("%s %s %s",
-			configPathStr, errpath(pathSlice), isNotValidStr),
+			configPathStr, mgmterror.ErrPath(pathSlice), isNotValidStr),
 			te.setMsg,
 		}
 	}
 
 	return []string{fmt.Sprintf("%s %s %s",
-		configPathStr, errpath(pathSlice), te.setSuffix),
+		configPathStr, mgmterror.ErrPath(pathSlice), te.setSuffix),
 		te.setMsg,
 	}
 }
